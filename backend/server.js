ce76900b7ce5d1e02efe381e296857c544670ba3
chien2734 <buichienthpthoa2004@gmail.com>
2026-03-17 15:07:56 +0700

    tạo backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const apiRoutes = require('./src/routes/index');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Sử dụng tất cả routes với tiền tố /api/v1
+app.use('/api/v1', apiRoutes);
+
+// Xử lý lỗi 404 (Không tìm thấy route)
+app.use((req, res, next) => {
+    res.status(404).json({ success: false, message: 'API Route không tồn tại' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server Backend đang chạy tại http://localhost:${PORT}`);
+});
