f1d23994c59d70d3b52c55ad034e746642d9abb4
TranKhaiAn <antrnkhai@gmail.com>
2026-05-07 02:30:39 +0700

    Ràng buộc điều chỉnh chấm công và thêm xem người duyệt trong hồ sơ

--- backend/src/modules/hr/hr.model.js
+++ backend/src/modules/hr/hr.model.js
@@ -821,9 +821,11 @@ const HrModel = {
     // 2. Nhân viên: Xem danh sách đơn mình đã nộp
     getLeaveRequestById: async (maNhanVien) => {
         const sql = `
-            SELECT * FROM dontu 
-            WHERE maNhanVien = ? 
-            ORDER BY ngayBatDau DESC
+            SELECT dt.*, nd.hoTen as tenNguoiDuyet
+            FROM dontu dt
+            LEFT JOIN nhanvien nd ON dt.maNguoiDuyet = nd.maNhanVien
+            WHERE dt.maNhanVien = ? 
+            ORDER BY dt.ngayBatDau DESC
         `;
         const [rows] = await db.query(sql, [maNhanVien]);
         return rows;
